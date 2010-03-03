7a66d96ccd6676299ae10ad9b010dbd0b3d7bfa5
nathforge <github@nreynolds.me.uk>
2010-03-03 11:39:31 +0000

    Added setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pysrc',
+      version='1.0',
+      description='Opens a Python module in a text editor - e.g, "pysrc django.core.management"',
+      author='Nathan Reynolds',
+      author_email='pypi@nreynolds.me.uk',
+      scripts=['pysrc'])
+
